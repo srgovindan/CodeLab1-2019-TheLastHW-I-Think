796cd5ced20c72973fd3512205763f6f8e718c97
Siddarth <siddarthrgovindan@gmail.com>
2019-04-26 20:56:03 -0400

    init project, already been working on main scene for a while

---
+++ PiedPiper/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager GM;
+ 
+    void Start()
+    {
+        // SINGLETON
+        if (GM == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            GM = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ PiedPiper/Assets/Scripts/VisionConeFollowPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionConeFollowPlayer : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody rb;
+    public Transform patrolPointA;
+    public Transform patrolPointB;
+    
+    public float speed;
+    
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //get unit vector between player and me
+        Vector3 direction = Vector3.Normalize(player.transform.position - transform.position);
+        //get look direction
+        Quaternion quatDirection = Quaternion.LookRotation(direction);
+        //get dot product between player and enemy 
+        float dotProduct = Vector3.Dot(direction, transform.forward);
+        
+        //debug draw
+        Debug.DrawRay(transform.position, transform.forward * 5, Color.cyan);
+        Debug.DrawRay(transform.position, direction * 5, Color.green);
+        
+        
+        //follow player in sight
+        if (dotProduct > 0.25)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, quatDirection, Time.deltaTime);
+            rb.velocity = transform.forward * speed;
+        }
+        
+        //patrol if player is not in sight
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
